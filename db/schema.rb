be9e683184212014e1035e29c211d9805255231b
George Wiggin <george@sequest.io>
2022-05-24 16:46:49 +0100

    all green

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_105118) do
+ActiveRecord::Schema[6.1].define(version: 2022_05_24_105118) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
